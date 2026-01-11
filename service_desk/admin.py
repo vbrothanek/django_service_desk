44eaf25c4e0725278404c25a4b25a24df4091521
Vojtech Brothanek <vbrothanek@brotel.cz>
2026-01-11 18:00:24 +0100

    Created new admin views for admin purposes.

--- service_desk/admin.py
+++ service_desk/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from .models import Firm, UserProfile, Ticket
 
 # Register your models here.
+@admin.register(Firm)
+class FirmAdmin(admin.ModelAdmin):
+    list_display = ['name', 'IC', 'city', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name', 'IC']
+
+
+
+# UserProfileInline > To be able to edit UserProfile from user settings.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [UserProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['ticket_number', 'subject', 'user' ,'created_at', 'assigned_to']
+    list_filter = ['ticket_number', 'created_at', 'assigned_to']
+    search_fields = ['ticket_number', 'subject']
