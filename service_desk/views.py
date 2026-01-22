fab9779bdba61b54851ba18bf2e531e2b73c7b8d
Vojtech Brothanek <vbrothanek@brotel.cz>
2026-01-22 20:51:08 +0100

    Add first fn for view.py and html template

--- service_desk/views.py
+++ service_desk/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Ticket, Company, Record
 
-# Create your views here.
+def ticket_list(request):
+    tickets = Ticket.objects.all()
+    context = {'tickets': tickets}
+
+    return render(request, 'service_desk/ticket_list.html')
