3e37b164efdb9ac3afbf76adde0af15f3919ca74
Vojtech Brothanek <vbrothanek@brotel.cz>
2026-02-05 20:47:04 +0100

    Created new views with login required for create new ticket

--- service_desk/views.py
+++ service_desk/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Ticket, Company, Record
 from django.contrib.auth.decorators import login_required
+from service_desk.forms import TicketForm
 
 def ticket_list(request):
     all_tickets = Ticket.objects.all()
@@ -18,10 +19,33 @@ def dashboard_view(request):
 def login_view(request):
     return render(request, 'service_desk/login.html')
 
+@login_required
 def tickets_view(request):
     all_tickets = Ticket.objects.all()
     latest_tickets = all_tickets.order_by('-last_update').filter()[:5]
-    return render(request, 'service_desk/tickets.html', {'all_tickets': all_tickets,'latest_tickets': latest_tickets})
+    user_tickets = Ticket.objects.filter(user=request.user)
 
+    context = {
+        'latest_tickets': latest_tickets,
+        'user_tickets': user_tickets,
+        'all_tickets': all_tickets
+    }
+
+    return render(request, 'service_desk/tickets.html', context)
+
+@login_required
 def create_ticket_view(request):
-    return render(request, 'service_desk/create_ticket.html')
+
+    if request.method == 'GET':
+        form = TicketForm()
+    else:
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            print(instance.ticket_number)
+            return redirect('service_desk:tickets')
+
+
+    return render(request, 'service_desk/create_ticket_form.html', {'form': form})
