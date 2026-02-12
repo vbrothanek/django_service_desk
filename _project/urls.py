92d59ab3f05fa0b2af8f9b536edfe38477714b52
Vojtech Brothanek <vbrothanek@brotel.cz>
2026-02-12 18:27:24 +0100

    Added new auth library allauth

--- _project/urls.py
+++ _project/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('service-desk/',include('service_desk.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ] + debug_toolbar_urls() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
