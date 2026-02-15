50f615c59db92e49e3d5d389e61dfe73b1a30d1a
Vojtech Brothanek <vbrothanek@brotel.cz>
2026-02-15 20:19:43 +0100

    New views of tickets, tickets details, etc.

--- service_desk/views.py
+++ service_desk/views.py
@@ -1,5 +1,7 @@
+from django.db.models import OuterRef, Subquery, DateTimeField
+from django.utils import timezone
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Ticket, Company, Record
+from .models import Ticket, Company, Record, TicketReadStatus
 from django.contrib.auth.decorators import login_required
 from service_desk.forms import TicketForm
 
@@ -9,31 +11,48 @@ def ticket_list(request):
 
     return render(request, 'service_desk/ticket_list.html', context)
 
+@login_required
 def default_view(request):
-    return redirect('service_desk:tickets')
+    return redirect('service_desk:dashboard')
 
-# @login_required
+
+@login_required
 def dashboard_view(request):
     context = {'active_page': 'dashboard'}
     return render(request, 'service_desk/dashboard.html', context)
 
-def login_view(request):
-    return render(request, 'service_desk/login.html')
 
 @login_required
 def tickets_view(request):
-    all_tickets = Ticket.objects.all()
-    latest_tickets = all_tickets.order_by('-last_update').filter()[:5]
-    user_tickets = Ticket.objects.filter(user=request.user)
+    user_groups = request.user.groups.values_list('name', flat=True)
+
+    if 'Agents' in user_groups:
+        tickets = Ticket.objects.all()
+    else:
+        tickets = Ticket.objects.filter(
+            company__in = request.user.companies.all()
+        )
+
+    tickets = tickets.order_by('-last_update')
+
+    read_statuses = dict(
+        TicketReadStatus.objects.filter(
+            user=request.user).values_list('ticket_id',
+            'last_read_at'))
+
+    for ticket in tickets:
+        if ticket.pk not in read_statuses or ticket.last_update > read_statuses[ticket.pk]:
+            ticket.is_unread = True
+        else:
+            ticket.is_unread = False
 
     context = {
         'active_page': 'tickets',
-        'latest_tickets': latest_tickets,
-        'user_tickets': user_tickets,
-        'all_tickets': all_tickets
+        'tickets': tickets
     }
 
     return render(request, 'service_desk/tickets.html', context)
+
 
 @login_required
 def create_ticket_view(request):
@@ -50,7 +69,7 @@ def create_ticket_view(request):
             if request.POST.get('action') == 'save_and_another':
                 return redirect('service_desk:new_ticket')
             # print(instance.ticket_number)
-            return redirect('service_desk:tickets')
+            return redirect('service_desk:ticket_detail', ticket_number=instance.ticket_number)
 
     context = {
         'active_page': 'tickets_create',
@@ -59,8 +78,18 @@ def create_ticket_view(request):
 
     return render(request, 'service_desk/create_ticket_form.html', context)
 
+
 @login_required
 def ticket_detail_view(request, ticket_number):
     ticket = get_object_or_404(Ticket, ticket_number=ticket_number)
+
+    TicketReadStatus.objects.update_or_create(
+        ticket = ticket,
+        user = request.user,
+        defaults = {
+            'last_read_at': timezone.now(),
+        }
+    )
+
     context = {'ticket': ticket}
     return render(request, 'service_desk/ticket_detail.html', context)
