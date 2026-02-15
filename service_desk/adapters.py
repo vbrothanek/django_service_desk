9c474b6b6bd402f25cf18d54821f1db073dfe9be
Vojtech Brothanek <vbrothanek@brotel.cz>
2026-02-15 20:19:07 +0100

    Created adapter for disable singup possibility.

---
+++ service_desk/adapters.py
@@ -0,0 +1,9 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+class NoSignupAccountAdapter(DefaultAccountAdapter):
+
+   def is_open_for_signup(self, request):
+       """
+       Not open for signup adapter.
+       """
+       return False
